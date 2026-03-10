4d73f4b30a4689c6b5e3d8ec1e999f43c151558f
Lars Kleen <lars.kleen@mhp.com>
2026-03-10 20:53:02 +0100

    add translation for about page

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TranslationService } from '../../core/services/translation';
 
 @Component({
   selector: 'app-about',
@@ -6,4 +7,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './about.html',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  private readonly i18n = inject(TranslationService);
+
+  protected readonly title = this.i18n.t('about.title');
+  protected readonly intro = this.i18n.t('about.intro');
+  protected readonly more = this.i18n.t('about.more');
+}
