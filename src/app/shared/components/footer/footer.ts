0ccaa1c041d0c6d172690bdc6529eb09f75d67c4
Lars Kleen <lars.kleen@mhp.com>
2026-03-10 20:48:50 +0100

    integrate i18n for menu and footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@a
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { ConsentService } from '../../../core/services/consent';
 import { LocaleService } from '../../../core/services/locale';
+import { TranslationService } from '../../../core/services/translation';
 import { filter } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
@@ -12,11 +13,12 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './footer.html',
 })
 export class FooterComponent {
-  protected consent = inject(ConsentService);
-  protected currentYear = new Date().getFullYear();
+  protected readonly consent = inject(ConsentService);
+  protected readonly currentYear = new Date().getFullYear();
 
   private readonly router = inject(Router);
   private readonly localeService = inject(LocaleService);
+  private readonly i18n = inject(TranslationService);
   private readonly currentUrl = signal(this.router.url);
 
   protected readonly currentLocale = computed(() =>
@@ -24,6 +26,10 @@ export class FooterComponent {
   );
   protected readonly impressumLink = computed(() => ['/', this.currentLocale(), 'impressum']);
   protected readonly datenschutzLink = computed(() => ['/', this.currentLocale(), 'datenschutz']);
+
+  protected readonly labelImpressum = this.i18n.t('footer.impressum');
+  protected readonly labelDatenschutz = this.i18n.t('footer.privacy-policy');
+  protected readonly labelCookiePreferences = this.i18n.t('footer.cookiePreferences');
 
   constructor() {
     this.router.events
