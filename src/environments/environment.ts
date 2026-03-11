246ec27b4fdcceaee17e8e77c73bb36e871638ec
Lars Kleen <lars.kleen@mhp.com>
2026-03-11 22:17:46 +0100

    add google analytics measurement id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ga4MeasurementId: 'G-XXXXXXXXXX', // ← replace with your real GA4 ID
+  ga4MeasurementId: 'G-X6Y9YT2SMN',
   blogBaseUrl: 'https://larsworks.de',
 };
